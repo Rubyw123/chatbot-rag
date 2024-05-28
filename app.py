7377cacefc123d16fefb628747060c35908475e6
Rubyw123 <yw6429@nyu.edu>
2024-05-28 05:16:52 +0000

    Added sidebar feature for chat history

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from llm_chains import load_normal_chain, chatChain
 from langchain.memory import StreamlitChatMessageHistory
-
+from utils import save_chat_history_json,get_timestamp,load_chat_history_json
+import yaml
+import os
+with open("config.yaml","r") as f:
+    config = yaml.safe_load(f)
 
 def load_chain(chat_history):
     return load_normal_chain(chat_history)
@@ -15,21 +19,55 @@ def set_send_input():
     st.session_state['send_input'] = True
     clear_input_field()
 
+def save_chat_history():
+    if st.session_state["history"] != []:
+        if st.session_state["session_key"] == "new_session":
+            st.session_state["new_session_key"] = get_timestamp() + ".json"
+            save_chat_history_json(st.session_state["history"],config["chat_history_path"] + st.session_state["new_session_key"])
+        else:
+            save_chat_history_json(st.session_state["history"],config["chat_history_path"] + st.session_state["session_key"])
+
+def track_index():
+    st.session_state["session_index_tracker"] = st.session_state["session_key"]
+
 def main():
+    # App title
     st.title("Local Chat App")
-    chat_container=st.container()
+    chat_container=st.container()   
+
+    # Sidebar
+    st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"] + os.listdir(config["chat_history_path"])
 
     if 'send_input' not in st.session_state:
+        st.session_state["session_key"] = "new_session"
         st.session_state['send_input'] = False
         st.session_state['user_question']=""
+        st.session_state["new_session_key"] = None
+        st.session_state["session_index_tracker"] = "new_session"
+    if st.session_state["session_key"] == "new_session" and st.session_state["new_session_key"] != None:
+        st.session_state["session_index_tracker"] = st.session_state["new_session_key"]
+        st.session_state["new_session_key"] = None
 
+
+    index = chat_sessions.index(st.session_state["session_index_tracker"])
+    st.sidebar.selectbox("Select a chat session", chat_sessions,key="session_key",index = index, on_change=track_index)
+
+    if st.session_state["session_key"] != "new_session":
+        st.session_state["history"] = load_chat_history_json(config["chat_history_path"]+st.session_state["session_key"])
+    else:
+        st.session_state["history"] = []
     chat_history = StreamlitChatMessageHistory(key="history")
     llm_chain = load_chain(chat_history)
     user_input = st.chat_input("Type your message here",key="user_input")
 
     #send_button = st.button("Send",key="send_button")
 
-
+    if chat_history.messages != []:
+        with chat_container:
+            st.write("Chat history:")
+            for message in chat_history.messages:
+                st.chat_message(message.type).write(message.content)
 
     with chat_container:
         if user_input:
@@ -40,11 +78,7 @@ def main():
             st.chat_message("ai").write(llm_response)
             user_input = None
     
-    if chat_history.messages != []:
-        with chat_container:
-            st.write("Chat history:")
-            for message in chat_history.messages:
-                st.chat_message(message.type).write(message.content)
+    save_chat_history() 
 
 if __name__ == "__main__":
     main()
